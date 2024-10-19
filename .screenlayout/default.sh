b4c40ca86c79648baf9ce3ebcfabdd0e5c6ac34c
Jakob Fischer <jakobfischer93@gmail.com>
2024-10-19 08:39:04 +0200

    Update screenres, tmux keybind

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 xrandr \
-	--output DP-4 \
+	--output HDMI-0 \
 		--primary \
-		--mode 3440x1440 \
-		--rate 100 \
+		--mode 5120x1440 \
+		--rate 240 \
 		--pos 0x0 \
 		--rotate normal \
 	--output DP-0 --off \
 	--output DP-1 --off \
 	--output DP-2 --off \
 	--output DP-3 --off \
-	--output DP-5 --off \
-	--output HDMI-0 --off
+	--output DP-4 --off \
+	--output DP-5 --off
